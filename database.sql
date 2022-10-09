3fc058da57706447578b9056b95a49e2bd73b50f
aleckrichard <alexrichard.alvarado@gmail.com>
2022-10-09 11:58:39 -0300

    Delete DB file

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS noderestapimysql;
-
-USE noderestapimysql;
-
-CREATE TABLE `employee` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(100) NULL,
-  `salary` int NULL
-);
